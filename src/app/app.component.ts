d1363dbf321393497e03d15417611c7a25c714cd
AGmaj <adrian.gmaj@gmail.com>
2023-09-09 17:32:22 +0200

    toDoList- tasks- list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'toDoList';
+
+  tasks: Array<string> = []
+  doneTasks: Array<string> = []
+  newTask: string = ''
+
+  add(task: string) {
+    this.tasks.unshift(task)
+    console.log(task)
+    this.newTask =''
+  }
+
+  remove(task: string) {
+    this.tasks = this.tasks.filter((e) => e != task)
+  }
+
+  done(task: string) {
+    this.doneTasks.push(task);
+    this.remove(task)
+  }
 }
